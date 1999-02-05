d549aa5d647f672965788fd688e093205a515429
Slava Pestov <sp@gjt.org>
1999-02-05 22:12:15 +0000

    Replace in selection and select line range commands

---
+++ org/gjt/sp/jedit/gui/SelectLineRange.java
@@ -0,0 +1,143 @@
+/*
+ * SelectLineRange.java - Selects a range of lines
+ * Copyright (C) 1999 Slava Pestov
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.gjt.sp.jedit.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.text.Element;
+import javax.swing.*;
+import org.gjt.sp.jedit.*;
+
+public class SelectLineRange extends JDialog
+implements ActionListener, KeyListener
+{
+	public SelectLineRange(View view)
+	{
+		super(view,jEdit.getProperty("selectlinerange.title"),true);
+
+		buffer = view.getBuffer();
+		int lineNo = buffer.getDefaultRootElement().getElementIndex(
+			view.getTextArea().getCaretPosition()) + 1;
+		
+		getContentPane().add("North",new JLabel(jEdit.getProperty(
+			"selectlinerange.caption")));
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2,2));		
+		panel.add(new JLabel(jEdit.getProperty("selectlinerange.start"),
+			SwingConstants.RIGHT));
+		panel.add(start = new JTextField(String.valueOf(lineNo)));
+		start.addKeyListener(this);
+
+		panel.add(new JLabel(jEdit.getProperty("selectlinerange.end"),
+			SwingConstants.RIGHT));
+		panel.add(end = new JTextField(String.valueOf(lineNo)));
+		end.addKeyListener(this);
+		getContentPane().add("Center",panel);
+
+		panel = new JPanel();
+		panel.add(ok = new JButton(jEdit.getProperty("selectlinerange.ok")));
+		ok.addActionListener(this);
+		panel.add(cancel = new JButton(jEdit.getProperty(
+			"selectlinerange.cancel")));
+		cancel.addActionListener(this);
+		getContentPane().add("South",panel);
+
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		getRootPane().setDefaultButton(ok);
+		addKeyListener(this);
+		Dimension screen = getToolkit().getScreenSize();
+		pack();
+		setLocation((screen.width - getSize().width) / 2,
+			(screen.height - getSize().height) / 2);
+		show();
+		start.requestFocus();
+	}
+
+	public void actionPerformed(ActionEvent evt)
+	{
+		Object source = evt.getSource();
+
+		if(source == ok)
+			doSelectLineRange();
+		else if(source == cancel)
+			dispose();
+	}
+
+	public void keyPressed(KeyEvent evt)
+	{
+		switch(evt.getKeyCode())
+		{
+		case KeyEvent.VK_ENTER:
+			doSelectLineRange();
+			break;
+		case KeyEvent.VK_ESCAPE:
+			dispose();
+			break;
+		}
+	}
+
+	public void keyReleased(KeyEvent evt) {}
+	public void keyTyped(KeyEvent evt) {}
+
+	// private members
+	private Buffer buffer;
+	private JTextField start;
+	private JTextField end;
+	private JButton ok;
+	private JButton cancel;
+
+	private void doSelectLineRange()
+	{
+		int startLine;
+		int endLine;
+
+		try
+		{
+			startLine = Integer.parseInt(start.getText()) - 1;
+			endLine = Integer.parseInt(end.getText()) - 1;
+		}
+		catch(NumberFormatException nf)
+		{
+			getToolkit().beep();
+			return;
+		}
+
+		Element startElement = buffer.getDefaultRootElement()
+			.getElement(startLine);
+		Element endElement = buffer.getDefaultRootElement()
+			.getElement(endLine);
+		
+		if(startElement == null || endElement == null)
+		{
+			getToolkit().beep();
+			return;
+		}
+
+		int startOffset = startElement.getStartOffset();
+		int endOffset = endElement.getEndOffset();
+		if(endOffset > buffer.getLength())
+			endOffset = buffer.getLength();
+
+		((View)getParent()).getTextArea().select(startOffset,endOffset);
+
+		dispose();
+	}
+}
