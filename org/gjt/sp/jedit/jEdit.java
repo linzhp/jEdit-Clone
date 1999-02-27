fe58f684a99eb6b88ec95aae54351928dfecece3
Slava Pestov <sp@gjt.org>
1999-02-27 23:14:21 +0000

    More tips, build number updated

--- org/gjt/sp/jedit/jEdit.java
+++ org/gjt/sp/jedit/jEdit.java
@@ -46,7 +46,7 @@ public class jEdit
 	 * The date when a change was last made to the source code,
 	 * in <code>YYYYMMDD</code> format.
 	 */
-	public static final String BUILD = "19990227";
+	public static final String BUILD = "19990228";
 
 	/**
 	 * AWK regexp syntax.
