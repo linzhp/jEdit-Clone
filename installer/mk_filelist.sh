27b7675d7555a6546197697f3dde762a846f82cb
Slava Pestov <slava@jedit.org>
2001-06-12 08:49:05 +0200

    import of auxiliary files for jEdit-3.2pre3

--- installer/mk_filelist.sh
+++ installer/mk_filelist.sh
@@ -10,13 +10,15 @@ echo properties/example.props >> installer/jedit-program
 find modes -name \*.xml >> installer/jedit-program
 echo modes/catalog >> installer/jedit-program
 find doc -type f -name \*.txt >> installer/jedit-program
-echo doc/welcome.html >> installer/jedit-program
-find macros -name \*.bsh >> installer/jedit-program
-find doc/users-guide/ -name \*.html >> installer/jedit-program
-echo doc/users-guide/toc.xml >> installer/jedit-program
 
 echo -n "jedit-program: "
 ls -l `cat installer/jedit-program` | awk 'BEGIN { size=0 } { size+=$5 } END { print size / 1024 }'
+
+# jedit-macros fileset
+find macros -name \*.bsh > installer/jedit-macros
+
+echo -n "jedit-macros: "
+ls -l `cat installer/jedit-macros` | awk 'BEGIN { size=0 } { size+=$5 } END { print size / 1024 }'
 
 # jedit-firewall fileset (Java 2 only)
 echo jars/Firewall.jar > installer/jedit-firewall
